8e31455dbba25674546f219c2614fa8e9987fc2e
kou ikutetsu <coolandy345@gmail.com>
2021-08-15 22:30:31 +0900

    Modbus Connection check

--- Source code/main.py
+++ Source code/main.py
@@ -1,15 +1,30 @@
-#System module import
+
+# --------------------------------------------------------------------------- # 
+# System module import
+# --------------------------------------------------------------------------- # 
 from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Manager
+from modbusTcpServer import ModbusTcpServer
 
-#local module import
+# --------------------------------------------------------------------------- # 
+# local module import
+# --------------------------------------------------------------------------- # 
 from registor_manager import RegistorManager
+import time
+
+def hello():
+    while 1:
+        print("Hello")
+        time.sleep(1)
 
 if __name__ == "__main__":
 
-    SystemMemoryPool=Manager()
-    ns = SystemMemoryPool.Namespace()
-    RegistorManager.loadMemoryPool(ns)
-
+    #Memory Prepare
+    MemoryPoolManager=Manager()
+    MemoryPool = MemoryPoolManager.dict()
+    RegistorManager.loadMemoryPool(MemoryPool)
+    
     with ProcessPoolExecutor(max_workers=10) as executor:
-        print("hello")
+        executor.submit(ModbusTcpServer.run_async_server)
+        executor.submit(hello)
+        print(MemoryPool)
--- Source code/registor_manager/RegistorManager.py
+++ Source code/registor_manager/RegistorManager.py
@@ -1,14 +1,12 @@
 import sqlite3
-from multiprocessing import Manager
 
-def loadMemoryPool(namespace):
+
+def loadMemoryPool(memoryPool):
 
     System_Registor_Database = sqlite3.connect('../Database and Profile/System Registor Structure Database.db')
     cur = System_Registor_Database.cursor()
-    
-    Dict={}
-    for row in cur.execute('SELECT * FROM "System Memory Pool"'):
-        Dict[row[0]]=[row[1],row[2]]
+   
+    for row in cur.execute('SELECT * FROM "System Memory Pool" '):
+        memoryPool[row[0]]=[row[1],row[2]]
 
-    print(Dict)
-    print(Dict["CR1"])
+    
