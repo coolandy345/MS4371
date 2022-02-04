2e5ddb61d94ab23e1f881aab41b0770b3472449b
KOU <coolandy345@gmail.com>
2022-02-04 16:32:42 +0900

    v1.4.1 release

--- Source code/main2.py
+++ Source code/main2.py
@@ -1,11 +1,3 @@
-import time
-source = [0,2,3,1,4,5] * 1000000
-lookup_table = [11,22,33,44,55,66]
-result = []
-start = time.time()
-for element in source:
-    transformed_value = lookup_table[element]
-    result.append(transformed_value)
-end = time.time()
-print(result[:5]) # [11,33,44,22,55]
-print(end - start) # ~2.5 seconds
+import os
+print(os.getenv('APPDATA'))
+MS4371\database
